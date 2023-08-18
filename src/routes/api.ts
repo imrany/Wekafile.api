a3bd98911e3277c8906d816bc15b1fe6782797f1
imrany <imranmat254@gmail.com>
2023-08-18 15:29:24 +0300

    defining api routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,13 @@
 import express from "express"
+import { deleteUser, getUserDetails, getUsers, loginUser, protectUser, registerUser, updateUser, verifyEmail } from "../controllers/user"
 const router=express.Router()
 
-router.get("/register",)
+router.post("/verify/:email",verifyEmail)
+router.post("/auth/register",registerUser)
+router.post("/auth/login",loginUser)
+router.get("/accounts/:email",protectUser,getUserDetails)
+// router.get("/accounts",getUsers)
+router.patch("/accounts/:email",protectUser,updateUser)
+router.delete("/accounts/:email",protectUser,deleteUser)
 
 export default router
