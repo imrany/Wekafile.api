2897e00415202effe175a41e64b2e486422d1594
imrany <imranmat254@gmail.com>
2023-08-18 11:14:37 +0300

    importing websocket into api

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from "express"
 import { config } from "dotenv"
-import path from "path"
 import cors from "cors"
+import socket from "./websocket"
 config()
 
 const cors_option = {
-    origin: "*",
-    methods: ["GET", "POST"]
+    origin:["http://localhost:8000","http://localhost:3001"],
+    methods: ["GET", "POST", "DELETE", "PATCH", "PUT"]
  }
 
 const app =express()
@@ -14,11 +14,8 @@ const app =express()
 app.use(express.static(`views`));
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cors({}))
+app.use(cors(cors_option))
 
-app.get("/*",(req,res)=>{
-    res.sendFile(path.resolve("views","index.html"))
-})
 
 const port=process.env.PORT||8000
 const server=app.listen(port,()=>{
@@ -34,35 +31,4 @@ let io = require("socket.io")(server,{
     },
     allowEIO3: true
 });
- 
-let users:any=[{userid:"",photo:"",platform:""}]
-io.on("connection", function(socket: any) {
-    var clientIp = socket.request.connection.remoteAddress;
-    console.log(`a user connected: ${socket.id}, ClientIP : ${clientIp} `);
-
-    socket.on("peers",(client_id:any)=>{
-        let user
-        for (let index = 0; index < users.length; index++) {
-             user = users[index];
-        }
-        if(client_id.userid!==user.userid){
-            users.push(client_id)
-        }
-        console.log(users.slice(1,users.length))
-        socket.emit("peers",users.slice(1,users.length))
-    })
-
-    socket.on("upload", (file:any, err:any) => {
-        console.log(file);  
-        if(err){
-            console.log(err)
-        }
-        socket.broadcast.emit("download",file)
-    });
-
-    //disconnect
-    socket.on("disconnect", (reason:any) => {
-        socket.emit(`${users.id} has left`)
-    });
-});
-  
+socket(io);
