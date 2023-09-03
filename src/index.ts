9bdbfbffd05deb238e6117406917c0d6dd2fd3c7
imrany <imranmat254@gmail.com>
2023-09-03 23:06:13 +0300

    working on the new approach on uploading and storing user files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express"
 import { config } from "dotenv"
+import multer from "multer"
 import cors from "cors"
+import {readdir, mkdir} from "fs"
 import socket from "./websocket"
 import router from "./routes/api"
 config()
@@ -11,13 +13,51 @@ const cors_option = {
 }
 
 const app =express()
+const path=`./uploads`
+const storage=multer.diskStorage({
+    destination:(req:any,file:any,callback:any)=>{
+        callback(null,path)
+    },
+    filename:(req:any,file:any,callback:any)=>{
+        callback(null,file.originalname)
+    }
+})
+const upload=multer({storage:storage})
+
 // app.set('view engine','ejs');
 app.use(express.static(`views`));
+app.use(express.static(`uploads`));
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors(cors_option))
 app.use("/api",router)
 
+//routes
+app.post("/upload",upload.array("files"),async(req:any,res:any)=>{
+    try {
+        console.log(req.files)
+        res.status(200).send({msg:"File received"})
+    } catch (error:any) {
+        res.status(505).send({error:error.message})
+    }
+})
+
+app.get("/read_file",async(req:any,res:any)=>{
+    try {
+        readdir(path,"utf8",(err:any,files)=>{
+            if(err){
+                console.log(err)
+                mkdir(path,()=>{
+                    console.log(`uploads dir made`)
+                })
+            }else{
+                res.send(files)
+            }
+        })
+    } catch (error:any) {
+        res.status(505).send({error:error.message})
+    }
+})
 
 const port=process.env.PORT||8080
 const server=app.listen(port,()=>{
