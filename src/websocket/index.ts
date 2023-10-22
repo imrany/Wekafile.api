353a90191573370396a98984eaf252c662c7eb3c
imrany <imranmat254@gmail.com>
2023-10-22 05:11:27 +0300

    added update socket listener that would listen to updates and send them to all users on wekafile

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -1,21 +1,12 @@
 import pool from "../pg";
 
 export default function socket(io:any){
-    let users:any=[{userid:"",photo:"",platform:""}]
     io.on("connection", function(socket: any) {
         var clientIp = socket.request.connection.remoteAddress;
         console.log(`a user connected: ${socket.id}, ClientIP : ${clientIp} `);
 
-        socket.on("fetch_from_sharedfiles", (email:string, err:any) => {
-            pool.query('SELECT filename,email,file,uploadedAt,size,type,groupname FROM group_uploads WHERE email = $1 OR $1 = ANY(allowedEmails) OR privacy=false',[email], (error, results) => {
-                if (error) {
-                    console.log(error)
-                    socket.emit("response",{error:`Failed fetch shared files.`})
-                }else{
-                    socket.emit("response",{files:results.rows,count:results.rowCount})
-                }
-            })
-
+        socket.on("update", (update:string, err:any) => {
+            socket.emit("response",update)
             if(err){
                 console.log(err)
             }
@@ -23,7 +14,7 @@ export default function socket(io:any){
         
         //disconnect
         socket.on("disconnect", (reason:any) => {
-            socket.emit(`${users.id} has left`)
+            socket.emit(`${reason} has left`)
         });
     });
 }
